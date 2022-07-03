dfd77258a6bfe76a94296ddffa51c22421397a90
NgNgari <nngunje@gmail.com>
2022-07-03 08:55:15 -0700

    A program that prints; no use of printf & use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * return always 0
- *
+ * Return: always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
