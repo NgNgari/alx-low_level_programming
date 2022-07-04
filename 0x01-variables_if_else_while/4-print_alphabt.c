a7c5dfd87e81936aaa56f327d7bfb4ce3f1c52de
NgNgari <nngunje@gmail.com>
2022-07-04 13:57:03 -0700

    a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; 0 <= 9; num++)
+		printf("%d", num);
+	puts("");
+	return (0);
+}
